370649a7eaadfe04accfc977e007154633c11da7
jakehemmerle <jakehemmerle@protonmail.com>
2022-11-16 23:42:48 -0500

    add README, linting, tests, devdeps, scaffolding

---
+++ packages/core/src/tests.test.ts
@@ -0,0 +1,12 @@
+import should from "should";
+import "mocha";
+
+const { assert } = require("chai");
+
+describe("Core Tests", async () => {
+    before(async function () {
+    });
+    after(function () {
+    });
+    it(`done`, async () => { });
+});
