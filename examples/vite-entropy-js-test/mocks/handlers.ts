968a3ac38f56db5df0f026cb41efa8560dbf6863
benjamin <2502947+benschac@users.noreply.github.com>
2023-02-13 16:41:30 -0500

    configure vitest, add basic react test

---
+++ examples/vite-entropy-js-test/mocks/handlers.ts
@@ -0,0 +1,33 @@
+import { rest } from 'msw'
+
+// Mock Data
+export const posts = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'first post title',
+    body: 'first post body',
+  },
+  {
+    userId: 2,
+    id: 5,
+    title: 'second post title',
+    body: 'second post body',
+  },
+  {
+    userId: 3,
+    id: 6,
+    title: 'third post title',
+    body: 'third post body',
+  },
+]
+
+// Define handlers that catch the corresponding requests and returns the mock data.
+export const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(posts))
+  }),
+  rest.get('http://127.0.0.1:9944', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ message: 'hello world' }))
+  }),
+]
