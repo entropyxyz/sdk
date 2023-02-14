a804f73bffa2ab9695e19ac967f19387474a6c08
benjamin <2502947+benschac@users.noreply.github.com>
2023-02-14 11:44:12 -0500

    refactor isValidSubstrate address test

--- core/tests.test.ts
+++ core/tests.test.ts
@@ -54,14 +54,4 @@ describe('Core Tests', async () => {
       )
     }
   })
-
-  it(`isValidSubstrateAddress() is true for valid account`, async () => {
-    const aliceStash = '5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc'
-    assert.equal(isValidSubstrateAddress(aliceStash), true)
-  })
-
-  it(`isValidSubstrateAddress() is false for wrong account lengths`, async () => {
-    const invalidAccount = '5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsS' // shorter than allowed
-    assert.equal(isValidSubstrateAddress(invalidAccount), false)
-  })
 })
