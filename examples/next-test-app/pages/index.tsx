80eade65235a4928e77c6531ae2f8799da31e26e
benjamin <2502947+benschac@users.noreply.github.com>
2023-02-14 12:16:03 -0500

    core tests and working in browser, add to example

--- examples/next-test-app/pages/index.tsx
+++ examples/next-test-app/pages/index.tsx
@@ -2,10 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Entropy from '../../../dist/browser'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  console.log(Entropy)
   return (
     <>
       <Head>
