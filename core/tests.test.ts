6ff660b33f7d99408656e079e575d6befc808930
jakehemmerle <jakehemmerle@protonmail.com>
2023-02-16 11:40:18 -0500

    removed duplicate tests

--- core/tests.test.ts
+++ core/tests.test.ts
@@ -1,7 +1,7 @@
 import 'mocha'
 import Entropy from '.'
 import { spinChain, spinThreshold, sleep, removeDB } from '../testing-utils'
-import { readKey, isValidSubstrateAddress } from './utils'
+import { readKey } from './utils'
 const { assert } = require('chai')
 import { BigNumber, ethers } from 'ethers'
 
@@ -73,15 +73,4 @@ describe('Core Tests', async () => {
       )
     }
   })
-
-  it(`isValidSubstrateAddress() is true for valid account`, async () => {
-    const aliceStash = '5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc'
-    assert.equal(isValidSubstrateAddress(aliceStash), true)
-  })
-    
-  it(`isValidSubstrateAddress() is false for wrong account lengths`, async () => {
-    const invalidAccount = '5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsS' // shorter than allowed
-    assert.equal(isValidSubstrateAddress(invalidAccount), false)
-  })
-
 })
