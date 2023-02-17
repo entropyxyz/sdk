6d48bf5c36f308d39ffcd2e84678e9fece2d3733
jakehemmerle <jakehemmerle@protonmail.com>
2023-02-17 10:50:51 -0500

    PR cleanup and refactoring

--- threshold-server/index.ts
+++ threshold-server/index.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { SignatureLike } from '@ethersproject/bytes'
 import { sleep } from '../core/utils'
 import { ITransactionRequest } from './types'
@@ -10,58 +10,44 @@ export class ThresholdServer {
   /**
    * @alpha
    *
+   * @async
    * @remarks
    * Is a method of the {@link ThresholdServer} class
    *
    * @param {Array<string>} emsgs - encrypted messages to be sent to a validator node
    * @param {Array<string>} serversWithPort - IP/domain and port of the threshold server, seperated by ':'
    *
-   * @returns {Promise<void>} - void
+   * @returns {Promise<AxiosResponse<any, any>[]>}
    */
 
-  async sendKeys(emsgs: Array<string>, serversWithPort: Array<string>) {
-    const responses = []
-    for (let i = 0; i < serversWithPort.length; i++) {
-      responses.push(axios.post(`http://${serversWithPort[i]}/user/new`, emsgs[i], {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }))
-    }
-
-    await Promise.all(responses)
+  async sendKeys(
+    encryptedKeys: Array<string>,
+    serversWithPort: Array<string>
+  ): Promise<AxiosResponse<any, any>[]> {
+    return Promise.all(
+      serversWithPort.map(async (server, index) =>
+        sendHttpPost(`http://${server}/user/new`, encryptedKeys[index])
+      )
+    )
   }
 
-  
   /**
    * Submits the transaction request to the threshold server so its constraints can be validated
    *
    * @async
    * @param {ITransactionRequest} txReq
    * @param {string[]} serversWithPort IP/domain and port of the threshold server, separated by ':'
-   * @returns {Promise<Response[]>}
+   * @returns {Promise<AxiosResponse<any, any>[]>}
    */
   async submitTransactionRequest(
     txReq: ITransactionRequest,
     serversWithPort: string[]
-    ): Promise<Response[]> {
-    const responses = [];
-    for (const server of serversWithPort) {
-      try {
-        const response = axios.post(`http://${server}/user/tx`, txReq, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-        responses.push(response);
-      } catch (error) {
-        // handle the error here (e.g. log it, rethrow it, etc.)
-        console.error(`Error submitting transaction request to ${server}:`, error);
-        return Promise.reject(error);
-      }
-    }
-
-    return Promise.all(responses);
+  ): Promise<AxiosResponse<any, any>[]> {
+    return Promise.all(
+      serversWithPort.map(async (server) =>
+        sendHttpPost(`http://${server}/user/tx`, txReq)
+      )
+    )
   }
 
   /**
@@ -90,10 +76,30 @@ export class ThresholdServer {
       } catch (e) {
         status = 500
         sleep(3000)
-        console.log({ message: 'repolling for signature soon', status, i })
+        console.info({ message: 'repolling for signature soon', status, i })
       }
       i++
     }
     return Uint8Array.from(atob(postRequest.data), (c) => c.charCodeAt(0))
   }
+}
+
+/**
+ * Sends an HTTP POST request to the specified URL with the given data and headers
+ *
+ * @async
+ * @param url the URL to send the POST request to
+ * @param data the data to send in the request body
+ * @returns {Promise<AxiosResponse<any, any>>}
+ */
+async function sendHttpPost(
+  url: string,
+  data: any
+): Promise<AxiosResponse<any, any>> {
+  const headers = {
+    'Content-Type': 'application/json',
+  }
+  return axios.post(url, data, {
+    headers: headers,
+  })
 }
--- threshold-server/types.ts
+++ threshold-server/types.ts
@@ -1,12 +1,12 @@
 // This associates an architecture with a transaction request
 export enum Arch {
-    Evm = 'evm',
-    // not supported yet
-    Btc = 'btc',
+  Evm = 'evm',
+  // not supported yet
+  Btc = 'btc',
 }
 
 // ITransactionRequest is what the `/user/tx` endpoint expects to receive
 export interface ITransactionRequest {
-    arch: Arch,
-    transaction_request: string
+  arch: Arch
+  transaction_request: string
 }
