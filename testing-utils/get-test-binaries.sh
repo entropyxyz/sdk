8a53b642fee04f38b2b656c11783cf52ffafb76e
peg <ameba23@systemli.org>
2023-06-27 12:24:59 +0200

    Add script to get test binaries

---
+++ testing-utils/get-test-binaries.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -H "Authorization: token $GH_TOKEN" -H 'Accept: application/octet-stream' 'https://github.com/entropyxyz/entropy-core/releases/download/v0.0.5/entropy' -o testing-utils/test-binaries/entropy && chmod a+x testing-utils/test-binaries/entropy
+curl -H "Authorization: token $GH_TOKEN" -H 'Accept: application/octet-stream' 'https://github.com/entropyxyz/entropy-core/releases/download/v0.0.5/server' -o testing-utils/test-binaries/server && chmod a+x testing-utils/test-binaries/server
