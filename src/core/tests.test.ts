85963b390c11bc0f84cfd98c4363783ca390adb6
JesseAbram <33698952+JesseAbram@users.noreply.github.com>
2023-06-27 06:50:51 -0400

    Update to axum (#173)
    * update to axum
    
    * new keys
    
    * prettier
    
    * Update test with new keyshare size
    
    * Update CI script to use binaries from github release rather than testing.entropy.family
    
    * Try setting GH authorisation token in CI
    
    * Add script to get test binaries
    
    * Use test binaries from api-docs as temporary solution
    
    ---------
    
    Co-authored-by: jesse <jesse@entropy.wxy>
    Co-authored-by: peg <ameba23@systemli.org>

--- src/core/tests.test.ts
+++ src/core/tests.test.ts
@@ -36,7 +36,7 @@ describe('Core Tests', () => {
     }
     await sleep(9000)
     await modifyOcwPostEndpoint(
-      'ws://localhost:9945',
+      'ws://127.0.0.1:9945',
       'http://localhost:3002/signer/new_party'
     )
     entropy = await Entropy.setup(charlieSeed)
@@ -68,12 +68,12 @@ describe('Core Tests', () => {
     })
 
     // signing attempts should fail cause we haven't set constraints yet
-    try {
-      await entropy.sign(whitelisted_test_tx_req, false, 3)
-      throw new Error('Should have errored')
-    } catch (e: any) {
-      assert.equal(e.message, 'Not Found')
-    }
+    const no_constraint: any = await entropy.sign(
+      whitelisted_test_tx_req,
+      false,
+      3
+    )
+    assert.equal(no_constraint.length, 0)
 
     // set user's constraints on-chain
     const charlieStashEntropy = await Entropy.setup(charlieStashSeed)
@@ -83,12 +83,12 @@ describe('Core Tests', () => {
     )
 
     // signing should fail with a non-whitelisted tx requests
-    try {
-      await entropy.sign(non_whitelisted_test_tx_req, false, 3)
-      throw new Error('Should have errored')
-    } catch (e: any) {
-      assert.equal(e.message, 'Not Found')
-    }
+    const wrong_constraint: any = await entropy.sign(
+      non_whitelisted_test_tx_req,
+      false,
+      3
+    )
+    assert.equal(wrong_constraint.length, 0)
 
     // signing should work for whitelisted tx requests
     const signature: any = await entropy.sign(
