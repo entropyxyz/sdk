59496d982f0a188d31ae607932d71ffb693a0fc6
frankiebee <frankie.diamond@gmail.com>
2023-08-16 12:28:37 -1000

    wrap the crypto lib in a proxy so it waits for import before calling the functions

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,27 +1,39 @@
 let isImported = false
-let crypto
+let cryptoLib
 const res: any = {}
+loadCryptoLib()
 
 export const cryptoIsLoaded: Promise<void> = new Promise((resolve) => {
   res.resolve = resolve
 })
 
+
+export const crypto = new Proxy({}, {
+  get: (_, key) => {
+    return async (...params) => {
+      await cryptoIsLoaded
+      return cryptoLib[key](...params)
+    }
+  }
+})
+
+
 export async function loadCryptoLib () {
-  if (isImported) return crypto
+  if (isImported) return cryptoLib
   // if node enviroment load node library
   if (typeof window === 'undefined') {
-    crypto = await import(
+    cryptoLib = await import(
       '@entropyxyz/x25519-chacha20poly1305-nodejs'
     )
     isImported = true
     res.resolve(true)
-    return crypto
+    return cryptoLib
   } else {
-    crypto = await import(
+    cryptoLib = await import(
       '@entropyxyz/x25519-chacha20poly1305-web'
     )
     isImported = true
     res.resolve(true)
-    return crypto
+    return cryptoLib
   }
 }
