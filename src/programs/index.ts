0f513a944527458747bc285faf83c46b884124fa
jawndiego <jawndiego@gmail.com>
2023-08-25 09:36:17 -0400

    replaced Promise<Any> with Promise<EventRecord>

--- src/programs/index.ts
+++ src/programs/index.ts
@@ -5,20 +5,21 @@ import { Signer} from '../types'
 import { getWallet } from '../keys'
 import { SubmittableExtrinsic } from '@polkadot/api/types'
 import { getApi } from "../utils";
+import { EventRecord } from "@polkadot/types/interfaces";
 
 
 /**
  * @alpha
  * @remarks
- * This is the {@link Constraint} class
- * A class for interfacing with the V1 Entropy Constraint system
+ * This is the {@link ProgramManager} class
+ * A class for interfacing with the V1 Entropy Constraints system
  */
 export default class ProgramManager extends Extrinsic {
   /**
    * @alpha
    * @remarks
-   * This function is part of the {@link Constraints} class
-   * Creates an instance of Constraints.
+   * This function is part of the {@link ProgramManager} class
+   * Creates an instance of ProgramManager.
    *
    * @param {ApiPromise} substrate - The api object for an Entropy blockchain
    * @param {Signer} signer - The signer object for the user interfacing with the Entropy blockchain
@@ -50,7 +51,7 @@ export default class ProgramManager extends Extrinsic {
     const tx: SubmittableExtrinsic<'promise'> = this.substrate.tx.constraints.setPrograms(entropyAccount, programs);
 
     await this.sendAndWaitFor(tx, true, {
-      section: 'constraints',
+      section: 'Programs',
       name: 'ProgramSet' 
     });
   }
@@ -63,11 +64,11 @@ export default class ProgramManager extends Extrinsic {
     accessControlInfo: any,
     entropyAccount: Address,
     freeTx?: boolean
-  ): Promise<any> {
+  ): Promise<EventRecord> {
     const tx: SubmittableExtrinsic<'promise'> = this.substrate.tx.constraints.updateConstraints(entropyAccount, accessControlInfo);
     return this.sendAndWaitFor(tx, freeTx || false, {
-      section: 'programs',
+      section: 'Programs',
       name: 'ProgramsUpdated',
     });
 
-  }}
+  }}    
