c52f9be9f3deee188b5050dfb1b172720e97530b
jawndiego <jawndiego@gmail.com>
2023-11-08 08:21:29 -0500

    renamed estimateUpdateProgramFee to estimateProgramDepositFee for more clarity

--- src/programs/index.ts
+++ src/programs/index.ts
@@ -140,7 +140,7 @@ export default class ProgramManager extends ExtrinsicBaseClass {
    * @alpha
    */
 
-  async estimateUpdateProgramFee (
+  async estimateProgramDepositFee (
     program: ArrayBuffer,
     programModificationAccount?: string,
     sigReqAccount: string = this.signer.wallet.address
