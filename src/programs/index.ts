d95ea8481e613f45b6e67562e9aa50eb678d152e
jawndiego <jawndiego@gmail.com>
2023-11-08 08:27:51 -0500

    programModificationAccount -> programModAccount for consistency in naming

--- src/programs/index.ts
+++ src/programs/index.ts
@@ -61,32 +61,32 @@ export default class ProgramManager extends ExtrinsicBaseClass {
    * Sets or updates the program of a specified account on Substrate
    * This method allows the current signer or an authorized account to update the program associated with the signer's account or another specified account.
    * @param {ArrayBuffer} program - The program to be set or updated, as an ArrayBuffer.
-   * @param {string} [programModificationAccount] - (Optional) The account that will be used to modify the program if different from the signer's account.
+   * @param {string} [programModAccount] - (Optional) The account that will be used to modify the program if different from the signer's account.
    * @param {string} [sigReqAccount=this.signer.wallet.address] - (Optional) The account for which the program will be set or updated. Defaults to the signer's account.
    * @returns {Promise<void>} A promise that resolves when the transaction has been included in the block.
    * @throws {Error} Throws an error if the account is unauthorized or if there's a problem setting the program.
    * @remarks
    * This method handles the conversion of a program from an ArrayBuffer to a hex string
-   * It checks for authorization if the programModificationAccount is provided, ensuring that only authorized accounts can update the bytecode.
+   * It checks for authorization if the programModAccount is provided, ensuring that only authorized accounts can update the bytecode.
    * The transaction is created and sent to Substrate. This method then awaits the confirmation event 'ProgramUpdated' to ensure that the update was successful.
    * @alpha
    */
 
   async set (
     program: ArrayBuffer,
-    programModificationAccount?: string, // optional param
+    programModAccount?: string, // optional param
     sigReqAccount: string = this.signer.wallet.address
   ): Promise<void> {
     try {
-      const accountToUse = programModificationAccount || sigReqAccount
-      if (programModificationAccount) {
+      const accountToUse = programModAccount || sigReqAccount
+      if (programModAccount) {
         const isAuthorized = await this.checkAuthorization(
           sigReqAccount,
-          programModificationAccount
+          programModAccount
         )
         if (!isAuthorized) {
           throw new Error(
-            `${programModificationAccount} is not authorized to modify the program for ${sigReqAccount}`
+            `${programModAccount} is not authorized to modify the program for ${sigReqAccount}`
           )
         }
       }
@@ -109,7 +109,7 @@ export default class ProgramManager extends ExtrinsicBaseClass {
   /**
    * Sets or revokes the authorization for a given account to modify the program associated with another account.
    * @param {string} sigReqAccount - The account for which the program will be modified.
-   * @param {string} programModificationAccount - The account that is being authorized or revoked.
+   * @param {string} programModAccount - The account that is being authorized or revoked.
    * @param {boolean} isAuthorized - Flag to set or revoke authorization.
    * @returns {Promise<void>}
    * @throws {Error} If there's an issue setting the authorization.
@@ -121,13 +121,13 @@ export default class ProgramManager extends ExtrinsicBaseClass {
 
   async setAuthorization (
     sigReqAccount: string,
-    programModificationAccount: string,
+    programModAccount: string,
     isAuthorized: boolean
   ): Promise<void> {
     try {
       const tx: SubmittableExtrinsic<'promise'> = this.substrate.tx.programs.setAuthorization(
         sigReqAccount,
-        programModificationAccount,
+        programModAccount,
         isAuthorized
       )
       await this.sendAndWaitFor(tx, false, {
@@ -163,7 +163,7 @@ export default class ProgramManager extends ExtrinsicBaseClass {
   /**
    * Estimate the transaction fee for updating a program.
    * @param {ArrayBuffer} program - The program to be set or updated, as an ArrayBuffer.
-   * @param {string} [programModificationAccount] - (Optional) The account that will be used to modify the program if different from the signer's account.
+   * @param {string} [programModAccount] - (Optional) The account that will be used to modify the program if different from the signer's account.
    * @param {string} [sigReqAccount=this.signer.wallet.address] - (Optional) The account for which the program will be set or updated. Defaults to the signer's account.
    * @returns {Promise<string>} - Estimated transaction fee in a human-readable format.
    * @throws {Error} If there's an issue with estimating the fee.
@@ -175,11 +175,11 @@ export default class ProgramManager extends ExtrinsicBaseClass {
 
   async estimateProgramDepositFee (
     program: ArrayBuffer,
-    programModificationAccount?: string,
+    programModAccount?: string,
     sigReqAccount: string = this.signer.wallet.address
   ): Promise<string> {
     try {
-      const accountToUse = programModificationAccount || sigReqAccount
+      const accountToUse = programModAccount || sigReqAccount
       const programHex = util.u8aToHex(new Uint8Array(program))
       const tx = this.substrate.tx.programs.updateProgram(
         accountToUse,
@@ -195,7 +195,7 @@ export default class ProgramManager extends ExtrinsicBaseClass {
   }
 }
 
-// i think the above implementation will require us to add programModificationAccount to register
-// before registering we then run setAuthorization on behalf of the sigReqAccount and assign a delegated programModificationAccount
-// then a programModificationAccount can set and update programs post register without having to do any set authorization extrinsics
-// may not be possible to set an authorization of a programModificationAccount on a sigReqAccount before it is even registered to entropy
+// i think the above implementation will require us to add programModAccount to register
+// before registering we then run setAuthorization on behalf of the sigReqAccount and assign a delegated programModAccount
+// then a programModAccount can set and update programs post register without having to do any set authorization extrinsics
+// may not be possible to set an authorization of a programModAccount on a sigReqAccount before it is even registered to entropy
