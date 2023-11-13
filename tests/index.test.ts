30fedc82b2f88d16ff6bc3c76f47e7c1e994bd19
jawndiego <jawndiego@gmail.com>
2023-11-13 01:31:03 -0500

    all test passing

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,22 +1,17 @@
 import { readFileSync } from 'fs'
-import { SignatureLike } from '@ethersproject/bytes'
 import Entropy from '../src'
 import {
   sleep,
   disconnect,
-  charlieSeed,
-  charlieAddress,
   charlieStashSeed,
   charlieStashAddress,
   whitelisted_test_tx_req,
-  non_whitelisted_test_tx_req,
-  whitelisted_test_constraints,
-  aliceSeed,
   aliceAddress
 } from './testing-utils'
 import { ethers } from 'ethers'
-import { keccak256 } from 'ethers/lib/utils'
-import { buf2hex, stripHexPrefix } from '../src/utils'
+import { buf2hex } from '../src/utils'
+import { Keyring } from '@polkadot/api'
+import { mnemonicGenerate } from '@polkadot/util-crypto'
 import { spawnSync } from 'child_process'
 
 describe('Core Tests', () => {
@@ -106,15 +101,7 @@ it('should handle registration, program management, and signing', async () => {
     const dummyProgram: any = readFileSync(
       './tests/testing-utils/template_barebones.wasm'
     )
-  const isAuthorized = await entropy.programs.checkAuthorization(charlieStashAddress, charlieStashAddress)
-  console.log("charlie, charlie:", await entropy.programs.checkAuthorization(charlieStashAddress, charlieStashAddress))
-  const notAuthorized = await entropy.programs.checkAuthorization(charlieStashAddress, aliceAddress)
-  console.log("alice, charlie:",await entropy.programs.checkAuthorization(charlieStashAddress, aliceAddress))
 
-  if (!isAuthorized) {
-    throw new Error(`${charlieStashAddress} is not authorized to modify the program.`)
-  }
-    console.log('Authorization check passed')
   
     console.log('setting program')
     await entropy.programs.set(dummyProgram, charlieStashAddress)
@@ -127,10 +114,17 @@ it('should handle registration, program management, and signing', async () => {
 
     let unauthorizedErrorCaught = false
 
+    const testMnemonic = mnemonicGenerate()
+    const keyring = new Keyring({ type: 'sr25519' })
+    const keypair = keyring.addFromUri(testMnemonic)
+
+    const derivedAddress = keypair.address
+    console.log('Derived Address:', derivedAddress)
+
     try {
       console.log("false program test")
-      await entropy.programs.set(dummyProgram, aliceAddress, charlieStashAddress)
-      fail('Alice should not be authorized to set the program for Charlie')
+      await entropy.programs.set(dummyProgram, derivedAddress, charlieStashAddress)
+      fail('derivedAddress should not be authorized to set the program for Charlie')
     } catch (error) {
       if (error.message.includes("Program modification account doesn't have permission to modify this program")) {
         unauthorizedErrorCaught = true
@@ -139,8 +133,8 @@ it('should handle registration, program management, and signing', async () => {
       }
     }
 
-    expect(unauthorizedErrorCaught).toBe(true)
-    
+    expect(unauthorizedErrorCaught).toBeTruthy()
+
     // signing attempts should fail cause we haven't set constraints yet
     /*    const no_constraint: any = await entropy.sign({
       sigRequestHash: keccak256(ethers.utils.serializeTransaction(whitelisted_test_tx_req)),
