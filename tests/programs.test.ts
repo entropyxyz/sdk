09fcaad3af609ee6b474c7b72c192fd97abe2319
jawndiego <jawndiego@gmail.com>
2023-11-19 17:33:06 -0500

    programs test passes

--- tests/programs.test.ts
+++ tests/programs.test.ts
@@ -17,55 +17,67 @@ import { mnemonicToSeed } from 'ethers/lib/utils'
 describe('Programs Tests', () => {
   let entropy: Entropy
 
+
   beforeAll(async () => {
     jest.setTimeout(300000) // Give us five minutes to spin up.
     try {
       spawnSync(
-        'docker',
-        ['compose', '--file', 'tests/docker-compose.yaml', 'up', '--detach'],
+        "docker",
+        [ "compose", "--file", "tests/docker-compose.yaml", "up", "--detach" ],
         { shell: true, stdio: 'inherit' } // Use shell's search path.
       )
     } catch (e) {
       console.error('Error in beforeAll: ', e.message)
     }
 
-
-
     const keyOptions = {
         seed: charlieStashSeed,
       }
-  
-      entropy = new Entropy({ keyOptions: keyOptions })
-      await entropy.ready
+
+    await sleep(30000) // Give the chain nodes some time to spin up.
+    entropy = new Entropy({ keyOptions: keyOptions })
+    await entropy.ready
   })
 
   afterAll(async () => {
     try {
       await disconnect(entropy.substrate)
       spawnSync(
-        'docker',
-        ['compose', '--file', 'tests/docker-compose.yaml', 'down'],
+        "docker",
+        [ "compose", "--file", "tests/docker-compose.yaml", "down" ],
+        { shell: true, stdio: 'inherit' }
+      )
+      spawnSync(
+        "docker",
+        [ "compose", "--file", "tests/docker-compose.yaml", "logs" ],
         { shell: true, stdio: 'inherit' }
       )
     } catch (e) {
-      console.error('Error in afterAll: ', e.message)
+        console.error('Error in afterAll: ', e.message)
     }
   })
+
 
   it('should handle programs', async () => {
     jest.setTimeout(60000)
 
+
+    const isRegistered = await entropy.isRegistered(
+        charlieStashAddress
+      )
+    if (!isRegistered) {  
     // Test registration
     console.log('pre-register')
-    try {
+ 
       await entropy.register({
         programModAccount: charlieStashAddress,
         keyVisibility: 'Permissioned',
         freeTx: false,
         initialProgram: '0x',
-      })
-    } catch (e) {
-      console.error('Error in registration:', e.message)
+  
+        }) 
+    
+        return await entropy.registrationManager.checkRegistrationStatus(charlieStashAddress)
     }
 
     // // create key to check authorized
@@ -90,10 +102,12 @@ describe('Programs Tests', () => {
 
 
   
-        console.log("false program test")
+    try {
         await entropy.programs.set(dummyProgram, derivedAddress)
-        expect('derivedAddress is not authorized to set the program for Charlie')
-
+        fail("Expected an error for unauthorized program set")
+      } catch (error) {
+        expect(error.message).toContain("doesn't have permission")
+      }
 
     // // pass an invalid program eventually
     // console.log('invalid program checks')
