12c3d18f357d51e5a1517b134e2fc50b02c3f806
jawndiego <jawndiego@gmail.com>
2023-11-28 01:36:51 -0500

    lint no longer displaying error

--- src/programs/index.ts
+++ src/programs/index.ts
@@ -2,7 +2,7 @@ import ExtrinsicBaseClass from '../extrinsic'
 import { ApiPromise } from '@polkadot/api'
 import { Signer } from '../types'
 import { SubmittableExtrinsic } from '@polkadot/api/types'
-import { hex2buf } from '../utils'
+import { hex2buf } from '../utils/'
 import * as util from '@polkadot/util'
 
 /**
