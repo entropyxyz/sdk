528984e6e5d3466dbfde7e5cca95babe47878a09
frankie <frankie.diamond@gmail.com>
2023-12-05 10:41:41 -0800

    wip: presign

--- src/signing/adapters/eth.ts
+++ src/signing/adapters/eth.ts
@@ -1,10 +1,11 @@
 import { utils } from 'ethers'
 import { Arch } from '../../types'
 import { keccak256 } from "ethereum-cryptography/keccak.js"
-
+import { stripHexPrefix } from '../../utils'
+import { encode } from '@ethereumjs/rlp'
 export async function preSign (tx): Promise<string> {
   // rlp encode
-  return await utils.serializeTransaction(tx)
+  return await encode(tx)
 }
 
 export async function postSign (sig: Uint8Array): Promise<string> {
@@ -16,11 +17,6 @@ export const type = 'eth'
 export const arch = Arch.Evm
 
 export function pubToAddress (publicKey: string): Promise<string> {
-  return new Promise(resolve => {
-    if (publicKey.startsWith('0x')) {
-      publicKey = publicKey.slice(2)
-    }
-    const hash = keccak256(Buffer.from(publicKey, 'hex'))
-    resolve('0x' + hash.slice(-40))
-  })
+  const hash = keccak256(Buffer.from(publicKey, 'hex'))
+  return `0x${hash.slice(-20)}`
 }
