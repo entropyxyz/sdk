c3f1126f36fafd8c7fdc96cbf55aa8be4c3b21ee
benjamin <benschac@gmail.com>
2023-12-05 21:27:59 -0500

    add entropy class type (#259)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+import  Entropy  from './../index';
 import { Keypair } from '@polkadot/util-crypto/types'
 import { KeyringPair } from '@polkadot/keyring/types'
 import type { AccountId32 } from '@polkadot/types/interfaces/runtime'
@@ -5,6 +6,7 @@ import { U8aFixed } from '@polkadot/types-codec'
 
 export type base64string = string
 export type hexString = string
+export type EntropyClass = InstanceType<typeof Entropy>
 
 export interface KeyPair {
   public: base64string
