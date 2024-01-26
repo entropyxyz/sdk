7640165b14d6e56ad774f2260a5c9dd737d428b0
jawndiego <jawndiego@gmail.com>
2024-01-25 21:46:13 -0500

    register update

--- src/registration/index.ts
+++ src/registration/index.ts
@@ -1,10 +1,12 @@
 import ExtrinsicBaseClass from '../extrinsic'
 import { Signer, Address } from '../types'
 import { ApiPromise } from '@polkadot/api'
+import { ProgramData } from '../programs'
+import {stringToU8a} from '@polkadot/util'
 
 export interface RegistrationParams {
   freeTx?: boolean
-  initialProgram?: string
+  initialProgram?: ProgramData
   keyVisibility?: 'Public' | 'Permissioned' | 'Private'
   programModAccount: Address
 }
@@ -22,16 +24,15 @@ export type KeyVisibilityInfo =
 /**
  * The `RegistrationManager` class provides functionality for user registration using the Polkadot/Substrate API.
  * It extends the `ExtrinsicBaseClass` to handle extrinsic submissions and utility methods.
- * 
+ *
  * Includes registering a user, checking if a user is already registered, and listening for registration events.
- * 
+ *
  */
 
 export default class RegistrationManager extends ExtrinsicBaseClass {
-  
   /**
    * Constructs a new instance of the `RegistrationManager` class.
-   * 
+   *
    * @param substrate - The Polkadot/Substrate API instance.
    * @param signer - The signer used for signing transactions.
    */
@@ -53,7 +54,7 @@ export default class RegistrationManager extends ExtrinsicBaseClass {
    * @param initialProgram - Optional. Initial program associated with the user.
    * @param keyVisibility - Key visibility level ('Public', 'Permissioned', 'Private'). Defaults to 'Permissioned'.
    * @param programModAccount - Account authorized to modify programs on behalf of the user.
-   * 
+   *
    * @returns A promise that resolves when the user is successfully registered.
    * @throws {Error} If the user is already registered.
    */
@@ -70,44 +71,64 @@ export default class RegistrationManager extends ExtrinsicBaseClass {
     // TODO: store multiple signers via address. and respond accordingly
     // however it should be handled in extrinsic class and not here
 
-    const isCurrentlyRegistered = await this.checkRegistrationStatus(this.signer.wallet.address)
+    const isCurrentlyRegistered = await this.checkRegistrationStatus(
+      this.signer.wallet.address
+    )
     if (isCurrentlyRegistered) {
       throw new Error('already registered')
     }
 
-    const registered: Promise<RegisteredInfo> = new Promise((resolve, reject) => {
-      try {
-        const unsubPromise = this.substrate.rpc.chain.subscribeNewHeads(
-          async () => {
-            const registeredCheck = await this.checkRegistrationStatus(this.signer.wallet.address)
-            if (registeredCheck) {
-              const unsub = await unsubPromise
-              unsub()
-              const registeredData = await this.substrate.query.relayer.registered(this.signer.wallet.address)
-              // @ts-ignore: next line
-              if (!registeredData.isSome) {
-                throw new Error('Registration information not found')
+    const registered: Promise<RegisteredInfo> = new Promise(
+      (resolve, reject) => {
+        try {
+          const unsubPromise = this.substrate.rpc.chain.subscribeNewHeads(
+            async () => {
+              const registeredCheck = await this.checkRegistrationStatus(
+                this.signer.wallet.address
+              )
+              if (registeredCheck) {
+                const unsub = await unsubPromise
+                unsub()
+                const registeredData = await this.substrate.query.relayer.registered(
+                  this.signer.wallet.address
+                )
+                // @ts-ignore: next line
+                if (!registeredData.isSome) {
+                  throw new Error('Registration information not found')
+                }
+                // @ts-ignore: next line
+                const data = registeredData.unwrap()
+                resolve({
+                  keyVisibility: data.keyVisibility.toJSON() as KeyVisibilityInfo,
+                  verifyingKey: data.verifyingKey.toString(),
+                })
               }
-              // @ts-ignore: next line
-              const data = registeredData.unwrap()
-              resolve({
-                keyVisibility: data.keyVisibility.toJSON() as KeyVisibilityInfo, 
-                verifyingKey: data.verifyingKey.toString(),
-              })
             }
-          }
-        )
-      } catch (e) {
-        reject(e)
+          )
+        } catch (e) {
+          reject(e)
+        }
       }
-    })
+    )
+
+    // Convert the ProgramData to PalletRelayerProgramInstance and wrap it in an array
+    const initialProgramParam = initialProgram
+      ? [
+        {
+          programPointer: initialProgram.hash,
+          programConfig: stringToU8a(JSON.stringify(initialProgram.config))
+          , 
+        },
+      ]
+      : []
 
     const registerTx = this.substrate.tx.relayer.register(
       programModificationAccount,
       keyVisibility,
-      initialProgram ? initialProgram : null
+      initialProgramParam 
     )
-    await this.sendAndWaitFor(registerTx, freeTx, {
+
+    await this.sendAndWaitFor (registerTx, freeTx, {
       section: 'relayer',
       name: 'SignalRegister',
     })
@@ -117,7 +138,7 @@ export default class RegistrationManager extends ExtrinsicBaseClass {
 
   /**
    * Verifies the registration status of an address.
-   * 
+   *
    * @param address - The address for which registration status needs to be checked.
    * @returns A promise which resolves to `true` if the address is registered, otherwise `false`.
    */
@@ -126,5 +147,4 @@ export default class RegistrationManager extends ExtrinsicBaseClass {
     const isRegistered = await this.substrate.query.relayer.registered(address)
     return !!isRegistered.toJSON()
   }
-
 }
