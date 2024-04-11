28e3025b7d42952fb900b371d70a5de358e4c960
frankie <frankie.diamond@gmail.com>
2024-04-11 11:44:30 -1000

    update scripts

--- dev/bin/generate-types.sh
+++ dev/bin/generate-types.sh
@@ -1,17 +1,14 @@
 #! /usr/bin/env bash
-source ./dev/bin/ENTROPY_CORE_VERSION.sh
-# create entropy metadata file
-touch entropy-metadata.json
-# spin up single node
-docker_file=./dev/docker-scripts/one-node-no-tss.yaml
-docker compose --file $docker_file up --detach
+
+
 # pull meta data write to file
-curl -H \"Content-Type: application/json\" -d '{\"id\":\"1\", \"jsonrpc\":\"2.0\", \"method\": \"state_getMetadata\", \"params\":[]}' http://localhost:9944 > entropy-metadata.json,
+curl -H "Content-Type: application/json" -d '{"id":"1", "jsonrpc":"2.0", "method": "state_getMetadata", "params":[]}' http://127.0.0.1:9944 > entropy-metadata.json &&
 # generate types
 
-ts-node --skip-project node_modules/.bin/polkadot-types-from-defs --package substrate-generated-types/interfaces --input ./substrate-generated-types/interfaces --endpoint ./entropy-metadata.json
-ts-node --skip-project node_modules/.bin/polkadot-types-from-chain --package substrate-generated-types/interfaces --endpoint ./entropy-metadata.json --output ./substrate-generated-types/interfaces
+# node --experimental-specifier-resolution=node --loader ts-node/esm  node_modules/.bin/polkadot-types-from-defs --package dscp-matchmaker-api/interfaces --input ./node_modules/@polkadot/types/ --endpoint ./entropy-metadata.json &&
+# node --experimental-specifier-resolution=node --loader ts-node/esm  node_modules/.bin/polkadot-types-from-chain --endpoint ./entropy-metadata.json --output ./substrate-generated-types/interfaces
 
-# spin down node
-docker compose --file $docker_file down
-docker compose --file $docker_file logs
+# "generate:defs":
+ts-node --skip-project node_modules/.bin/polkadot-types-from-defs --package ./node_modules/@polkadot/types/ --input ./substrate-generated-types --endpoint entropy-metadata.json
+# "generate:meta":
+ts-node --skip-project node_modules/.bin/polkadot-types-from-chain --package ./node_modules/@polkadot/types/ --endpoint entropy-metadata.json --output ./substrate-generated-types
