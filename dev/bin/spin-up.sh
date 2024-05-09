2da749b1b7652a897f1ba6ed9b1f7b4bb863dcfa
mixmix <mix@protozoa.nz>
2024-05-09 21:26:57 +1200

    quiet-pull

--- dev/bin/spin-up.sh
+++ dev/bin/spin-up.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 source ./dev/bin/ENTROPY_CORE_VERSION.sh
 
+env
+
 docker_file=dev/docker-scripts/$1.yaml
-docker compose --file $docker_file up --detach
+docker compose --file $docker_file up --detach --quiet-pull
