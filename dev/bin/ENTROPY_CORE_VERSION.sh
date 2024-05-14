165e33d40577217323158c2b97cf99ab5b431200
jawndiego <jawndiego@gmail.com>
2024-05-14 12:03:42 -0600

    docker image

--- dev/bin/ENTROPY_CORE_VERSION.sh
+++ dev/bin/ENTROPY_CORE_VERSION.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
 
-export ENTROPY_CORE_VERSION=entropy-chain-spec-v0.0.12-rc3.beta-9f844ee0c64f1a0627beaab54795772b443da0f7
-
+export ENTROPY_CORE_VERSION=test-vi-release-v0.0.12-rc3.beta
