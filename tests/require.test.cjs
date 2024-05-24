231601733aa54467395003634a18355f6105bd09
frankie <frankie.diamond@gmail.com>
2024-05-23 14:48:10 -1000

    turn require test back on

--- tests/require.test.cjs
+++ tests/require.test.cjs
@@ -1,15 +1,14 @@
 const test = require('tape')
-// const { default: Entropy, wasmGlobalsReady } = require('@entropyxyz/sdk')
-// const { default: Keyring } = require('@entropyxyz/sdk/keys')
+const { default: Entropy, wasmGlobalsReady } = require('@entropyxyz/sdk')
+const { default: Keyring } = require('@entropyxyz/sdk/keys')
 
-// const charlieSeed = '0xbc1ede780f784bb6991a585e4f6e61522c14e1cae6ad0895fb57b9a205a8f938'
+const charlieSeed = '0xbc1ede780f784bb6991a585e4f6e61522c14e1cae6ad0895fb57b9a205a8f938'
 
 test('CJS require', async t => {
-  t.skip('TODO - get require working again')
 
-  /*
   try {
     await wasmGlobalsReady()
+    console.log(wasmGlobalsReady)
     const keyring = new Keyring({ seed: charlieSeed })
     t.deepEqual(typeof keyring, 'object', 'keyring')
 
@@ -20,7 +19,6 @@ test('CJS require', async t => {
   } catch (err) {
     t.error(err, 'no errors')
   }
-  */
 
   t.end()
 })
