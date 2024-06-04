b640561e98f3bd4763cca7d6a2977a41b18318f1
Nayyir Jutha <nayyir@entropy.xyz>
2024-06-04 10:40:29 -0400

    added new test for getting owned programs

--- tests/programs.test.ts
+++ tests/programs.test.ts
@@ -1,57 +1,81 @@
 import test from 'tape'
-// import { readFileSync } from 'fs'
+import { readFileSync } from 'fs'
+import Entropy, { wasmGlobalsReady } from '../src'
+import Keyring from '../src/keys'
+import * as util from '@polkadot/util'
 
-// import Entropy from '../src'
-// import { buf2hex } from '../src/utils'
-// import {
-//   promiseRunner,
-//   spinNetworkUp,
-//   createTestAccount,
-//   spinNetworkDown,
-// } from './testing-utils'
+import {
+  sleep,
+  promiseRunner,
+  spinNetworkUp,
+  charlieStashSeed,
+  charlieStashAddress,
+  spinNetworkDown,
+} from './testing-utils'
 
-// const networkType = 'two-nodes'
-// let entropy: Entropy
+const networkType = 'two-nodes'
 
-// async function testTeardown() {
-//   await spinNetworkDown(networkType, entropy).catch((err) => {
-//     console.error('Error while spinning network down', err.message)
-//   })
-// }
-
-test('Programs', async (t) => {
-  t.skip('TODO - fix or delete')
-  /*
+test('Programs: GET', async (t) => {
   const run = promiseRunner(t)
 
   await run('network up', spinNetworkUp(networkType))
-  entropy = await run('account', createTestAccount(entropy))
-  t.teardown(testTeardown)
 
-  // await sleep(60000)
+  await sleep(process.env.GITHUB_WORKSPACE ? 30_000 : 5_000)
 
-  const dummyProgram = readFileSync(
-    './tests/testing-utils/template_barebones.wasm'
+  t.teardown(async () => {
+    await spinNetworkDown(networkType, entropy).catch(error => console.error('Error spinning network down', error.message))
+  })
+
+  await run('wasm', wasmGlobalsReady())
+
+  const keyring = new Keyring({ seed: charlieStashSeed, debug: true })
+  let store = keyring.getAccount()
+  t.equal(store.admin.address, keyring.accounts.registration.pair.address, 'admin account should have an address and for now it should match registrations address')
+  keyring.accounts.on('account-update', (fullAccount) => {
+    store = fullAccount
+  })
+
+  const entropy = new Entropy({
+    keyring,
+    endpoint: 'ws://127.0.0.1:9944',
+  })
+
+  // register
+  const verifyingKeyFromRegistration = await run('register', entropy.register())
+
+  t.equal(
+    verifyingKeyFromRegistration,
+    entropy.keyring.accounts.registration.verifyingKeys[0],
+    'verifyingKeys match after registration'
   )
-  const pointer = await run(
-    'deploy program',
-    entropy.programs.dev.deploy(dummyProgram)
+
+  // deploy
+  const noopProgram: any = readFileSync(
+    './tests/testing-utils/program_noop.wasm'
   )
 
-  const fetchedProgram = await run(
-    'get program',
-    entropy.programs.dev.get(pointer)
+  const newPointer = await run(
+    'deploy',
+    entropy.programs.dev.deploy(noopProgram)
+  )
+
+  const programsDeployed = await run(
+    'get deployed programs',
+    entropy.programs.dev.get(charlieStashAddress)
   )
 
   t.equal(
-    // @ts-ignore next line
-    buf2hex(fetchedProgram.bytecode),
-    buf2hex(dummyProgram),
-    'everything looks GREAT'
+    programsDeployed.toHuman().length,
+    1,
+    'charlie has deployed 1 program' + programsDeployed.toHuman()
+  )
+
+  t.equal(
+    programsDeployed.toHuman()[0],
+    newPointer,
+    'program in list matches new pointer: ' + newPointer + ' = ' + programsDeployed[0]
   )
 
   await entropy.close()
-  */
-
   t.end()
 })
