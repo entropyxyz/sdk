457b4c07a44c448cba7611afde12e7f99a8f959b
Nayyir Jutha <nayyir@entropy.xyz>
2024-06-17 15:22:38 -0400

    added tests to ensure sign still works with inputting own verifying key

--- tests/sign.test.ts
+++ tests/sign.test.ts
@@ -8,6 +8,7 @@ import {
   spinNetworkUp,
   spinNetworkDown,
   charlieStashSeed,
+  charlieSeed,
 } from './testing-utils'
 
 const NETWORK_TYPE = 'two-nodes'
@@ -47,6 +48,59 @@ test('Sign', async (t) => {
     entropy.signWithAdaptersInOrder({
       msg: { msg },
       order: ['deviceKeyProxy'],
+    })
+  )
+
+  t.true(signature && signature.length > 32, 'signature has some body!')
+  signature && console.log(signature)
+
+  t.end()
+})
+
+test('Sign: Inputted Verifying Keys', async (t) => {
+  const run = promiseRunner(t)
+
+  /* Setup Network */
+  await run('network up', spinNetworkUp(NETWORK_TYPE))
+  await sleep(process.env.GITHUB_WORKSPACE ? 30_000 : 5_000)
+  t.teardown(async () => {
+    // this gets called after all tests are run
+    await charlieStashEntropy.close()
+    await charlieEntropy.close()
+    await spinNetworkDown(NETWORK_TYPE).catch((error) =>
+      console.error('Error while spinning network down', error.message)
+    )
+  })
+
+  /* Setup Entropy */
+  await run('wasm', wasmGlobalsReady())
+  const charlieStashKeyring = new Keyring({ seed: charlieStashSeed, debug: true })
+  const charlieKeyring = new Keyring({ seed: charlieSeed, debug: true })
+  const charlieStashEntropy = new Entropy({
+    keyring: charlieStashKeyring,
+    endpoint: 'ws://127.0.0.1:9944',
+  })
+  const charlieEntropy = new Entropy({
+    keyring: charlieKeyring,
+    endpoint: 'ws://127.0.0.1:9944',
+  })
+  await run('charlieStashEntropy ready', charlieStashEntropy.ready)
+  await run('charlieEntropy ready', charlieEntropy.ready)
+  await run('charlie stash register', charlieStashEntropy.register())
+  await run('charlie register', charlieEntropy.register())
+
+
+  /* Sign */
+  const msg = Buffer
+    .from('Hello world: new signature from charlieStashEntropy!')
+    .toString('hex')
+
+  const signature = await run(
+    'sign',
+    charlieStashEntropy.signWithAdaptersInOrder({
+      msg: { msg },
+      order: ['deviceKeyProxy'],
+      signatureVerifyingKey: charlieEntropy.keyring.accounts.deviceKey.verifyingKeys[0]
     })
   )
 
