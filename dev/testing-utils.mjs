2d8e0845087530ed0a6f1b3db34885f0b015896f
mixmix <mix@protozoa.nz>
2024-10-11 11:49:44 +1300

    jumpStartNetwork timeout

--- dev/testing-utils.mjs
+++ dev/testing-utils.mjs
@@ -6,6 +6,7 @@ import { promisify } from 'util'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const moduleRoot = join(__dirname, '..')
+const SECONDS = 1000
 
 // NOTE: you need to edit your /etc/hosts file to use these. See dev/README.md
 
@@ -29,7 +30,7 @@ export async function spinNetworkUp (networkType = 'two-nodes') {
 async function retryUntil (fn, isSuccess = Boolean, opts = {}) {
   const {
     triesRemaining = process.env.GITHUB_WORKSPACE ? 30 : 10,
-    timeout = 1000
+    timeout = 1 * SECONDS
   } = opts
   return fn()
     .then(result => {
@@ -68,23 +69,26 @@ async function isWebSocketReady (endpoint) {
   })
 }
 
-export async function jumpStartNetwork (entropy) {
-  await entropy.substrate.tx.registry.jumpStartNetwork().signAndSend(entropy.keyring.accounts.registration.pair)
+export async function jumpStartNetwork (entropy, maxTime = 120 * SECONDS) {
+  let timeout, unsub
   const wantedMethod = 'FinishedNetworkJumpStart'
-  let unsub
-  // put a time out in here where at 2 minutes reject
-  await new Promise(async (res, reject) => {
-    unsub = await entropy.substrate.query.system.events((events) => {
-      events.forEach(async (record) => {
-        const { event } = record
-        const { method } = event
-        if (method === wantedMethod) {
-          unsub()
-          res(undefined)
-        }
-      })
+
+  const isDone = new Promise(async (res, reject) => {
+    timeout = setTimeout(reject, maxTime, new Error('jump-start network timed out'))
+
+    unsub = await entropy.substrate.query.system.events((records) => {
+      if (records.find(record => record?.event?.method === wantedMethod)) {
+        unsub()
+        clearTimeout(timeout)
+        res(undefined)
+      }
     })
   })
+
+  entropy.substrate.tx.registry.jumpStartNetwork()
+    .signAndSend(entropy.keyring.accounts.registration.pair)
+
+  await isDone
 }
 
 export async function spinNetworkDown (networkType = 'two-nodes') {
