037e6a014a5787e9379988cee547511d34e1136e
frankie <frankie.diamond@gmail.com>
2024-11-04 10:46:18 -1000

    cleanup boot script reporting

--- dev/deploy-facuets.mjs
+++ dev/deploy-facuets.mjs
@@ -6,20 +6,15 @@ import { jumpStartNetwork } from '@entropyxyz/sdk/testing'
 const endpoint = process.argv[2]
 const fundingSeed = process.argv[3]
 const faucetLookUpSeed = process.argv[4]
-const { ENTROPY_JUMPSTART } = process.env
-const pointer = '0x2eaf750c4fa0fe125ca8a9d4037c0c0608b57ae70d6586dc6acdfcb4e9872deb'
+const pointer = '0x3a1d45fecdee990925286ccce71f78693ff2bb27eae62adf8cfb7d3d61e142aa'
 if (!endpoint) throw new Error('please provide arguments for endpoint, fundingSeed, faucetLookUpSeed')
 if (!fundingSeed) throw new Error('please provide arguments for fundingSeed, faucetLookUpSeed')
 if (!faucetLookUpSeed) throw new Error('please provide arguments for faucetLookUpSeed')
-
+const report = {}
 checkEndpoint(endpoint)
 checkSeed(faucetLookUpSeed)
 checkSeed(fundingSeed)
 
-// const fundingSeed = '0x358f394d157e31be23313a1500f5e2c8871e514e530a35aa5c05334be7a39ba6'
-
-// const faucetLookUpSeed = '0xacdfa69a15317a1cf1b5ae13977f7274f4bdb20f9e38c2f1b107a693361fb80d'
-// const faucetLookUpAddress = '5F1uqENGb157HJt6J7bJHLgHyMuEw75VFsmXspdWDYNPXWuG'
 const faucetAddresses = []
 
 deployAndFundFaucet().then(() => process.exit(0)).catch((e) => { console.error(e); process.exit(1) })
@@ -42,23 +37,31 @@ async function deployAndFundFaucet () {
   })
   await faucetEntropy.ready
   await moneyBags.ready
+  const jumpStartStatus = (await moneyBags.substrate.query.stakingExtension.jumpStartProgress()).toHuman().jumpStartStatus
+  report['jump start status at start'] = jumpStartStatus
   // deploy faucet program to chain if not already up
-  if (ENTROPY_JUMPSTART) await jumpStartNetwork(moneyBags, true)
+  if (jumpStartStatus === 'Ready') await jumpStartNetwork(moneyBags, true)
   const faucetProgramInfo = await moneyBags.programs.dev.get(pointer)
+  report['using faucet program pointer'] = pointer
   if (faucetProgramInfo === null) {
     const faucetProgram = readFileSync(new URL('./faucet_program.wasm', import.meta.url))
     const configurationSchema = {
-      max_transfer_amount: "number",
-      genesis_hash: "string"
+      type: 'object',
+      properties: {
+        max_transfer_amount: { type: "number" },
+        genesis_hash: { type: "string" }
+      }
     }
     const auxDataSchema = {
-      amount: "number",
-      string_account_id: "string",
-      spec_version: "number",
-      transaction_version: "number",
+      type: 'object',
+      properties: {
+        amount: { type: "number" },
+        string_account_id: { type: "string" },
+        spec_version: { type: "number" },
+        transaction_version: { type: "number" },
+      }
     }
-    console.log('deploying faucet')
-    console.log('faucet pointer:', await moneyBags.programs.dev.deploy(faucetProgram, configurationSchema, auxDataSchema))
+    report['faucet program pointer from deployment'] = await moneyBags.programs.dev.deploy(faucetProgram, configurationSchema, auxDataSchema)
   }
   // transfer funds to faucet account enough to register (5 whole tokens)
   await sendMoney(moneyBags, faucetRing.accounts.registration.address, BigInt(5 * 10 ** 10))
@@ -68,14 +71,17 @@ async function deployAndFundFaucet () {
     max_transfer_amount: 20_000_000_000,
     genesis_hash: genesisHash.toString().split('0x')[1]
   }
-  console.log('faucet config:', userConfig)
+  report['faucet config'] = userConfig
   const faucets = []
   const funderBalance = BigInt((await faucetEntropy.substrate.query.system.account(
     moneyRing.accounts.registration.address)).data.free)
-  console.log('funderBalance', funderBalance)
+  report['initial balance for funding account'] = funderBalance
   const fundingAmount = funderBalance / BigInt(4)
+  report['initial funding faucet amount'] = fundingAmount
+
   while (!!faucetCount) {
     const vk = await faucetEntropy.register({
+      programModAddress: faucetEntropy.keyring.accounts.registration.address,
       programData: [{
         program_pointer: pointer,
         program_config: userConfig,
@@ -83,31 +89,22 @@ async function deployAndFundFaucet () {
     })
     const hashedKey = blake2AsHex(vk)
     const faucetAddress = encodeAddress(hashedKey, 42).toString()
-    console.log('funding faucet', vk)
-    sendMoney(moneyBags, faucetAddress, fundingAmount)
-    const balance = BigInt((await faucetEntropy.substrate.query.system.account(
-      faucetAddress)).data.free).toString()
+    await sendMoney(moneyBags, faucetAddress, fundingAmount)
+    const balance = (await faucetEntropy.substrate.query.system.account(
+      faucetAddress)).data.toHuman()
     faucets.push({
       vk,
       address: faucetAddress,
-      balance
+      balance: balance.free
     })
     --faucetCount
   }
   const modifiableKeys = await moneyBags.substrate.query.registry.modifiableKeys(faucetRing.accounts.registration.address)
-  console.log('modifiableKeys', modifiableKeys.toJSON(), 'for:', faucetRing.accounts.registration.address)
-  console.log(`
-    faucet look up address: ${faucetRing.accounts.registration.address}
-    faucets:
-
-      ${JSON.stringify(faucets[0])}
-
-      ${JSON.stringify(faucets[1])}
-
-      ${JSON.stringify(faucets[2])}
-    endpoint: ${endpoint}
-
-    `)
+  report['modifiableKeys on chain'] = modifiableKeys.toHuman()
+  report['faucet look up address'] = faucetRing.accounts.registration.address
+  report['faucets'] = faucets
+  report.endpoint = endpoint
+  console.log(report)
 }
 
 function sendMoney(entropy, recipientAddress, amount) {
@@ -115,7 +112,6 @@ function sendMoney(entropy, recipientAddress, amount) {
     // WARN: await signAndSend is dangerous as it does not resolve
     // after transaction is complete :melt:
     const sender = entropy.keyring.accounts.registration.pair
-    console.log(`transferring funds for registering from: ${sender.address} to: ${recipientAddress} for: ${amount}`)
     entropy.substrate.tx.balances
       .transferAllowDeath(recipientAddress, amount)
       .signAndSend(sender, ({ status, events, dispatchError }) => {
