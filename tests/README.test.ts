004257a1bee0f39bb04a6e6dac6656055f723544
frankie <frankie.diamond@gmail.com>
2025-01-07 11:16:39 -1000

    wip: test parser

---
+++ tests/README.test.ts
@@ -0,0 +1,77 @@
+import test from 'tape'
+import { readFileSync } from 'fs'
+import {
+  promiseRunner,
+  spinNetworkUp,
+  jumpStartNetwork,
+  eveSeed,
+  eveAddress,
+  spinNetworkDown,
+} from './testing-utils'
+import Entropy, { wasmGlobalsReady } from '../src'
+import Keyring from '../src/keys'
+
+
+const networkType = 'four-nodes'
+
+const msg = Buffer.from('Hello world: signature from entropy!').toString('hex')
+
+test.only('README basicExample', async (t) => {
+  const run = promiseRunner(t)
+  await run('network up', spinNetworkUp(networkType))
+  t.teardown(async () => {
+    await spinNetworkDown(networkType).catch((error) =>
+      console.error('Error while spinning network down', error.message)
+    )
+  })
+  await run('wasm', wasmGlobalsReady())
+
+  const eveKeyring = new Keyring({ seed: eveSeed, debug: true })
+
+  const jumpEntropy = new Entropy({
+    keyring: eveKeyring,
+    endpoint: 'ws://127.0.0.1:9944',
+  })
+
+  await run(
+    'entropy ready',
+    jumpEntropy.ready
+  )
+  await run('jump-start network', jumpStartNetwork(jumpEntropy))
+  await run('close jumpEntropy', jumpEntropy.close())
+  const README = readFileSync('./README.md', 'utf-8')
+  const codeExample = README.split('```typescript')[1].split('```')[0]
+
+  async function basicExample () {
+      // let wasam crypto libs load before use
+      await wasmGlobalsReady()
+      const eveSeed = '0x786ad0e2df456fe43dd1f91ebca22e235bc162e0bb8d53c633e8c85b2af68b7a'
+      const keyStore = { seed: eveSeed }
+      const keyring = new Keyring(keyStore)
+      const opts = {
+        endpoint: 'ws://127.0.0.1:9944', //defaults to 'ws://127.0.0.1:9944' if no value is provided.
+        keyring,
+      }
+      const entropy = new Entropy({keyring, endpoint})
+      await entropy.ready
+      // this is default program registration and configuration see deep dive docs for full explanation of function usage
+      const msgObject = {
+        msg: Buffer.from('Hello world: signature from entropy!').toString('hex'),
+      }
+      const verifyingKey = await entropy.register()
+      const signatureData = await entropy.signWithAdaptersInOrder({
+        msg: msgObject,
+        order: ['deviceKeyProxy'],
+      })
+      if (!await entropy.verify(signatureData)) throw new Error('can not verify signature')
+      const { signature } = signatureData
+      // do stuff with signature
+      /**your code here**/
+      // close websocket connection
+      await entropy.close()
+    }
+  await run('basicExample', basicExample())
+
+  t.ok(README)
+  t.end()
+})
